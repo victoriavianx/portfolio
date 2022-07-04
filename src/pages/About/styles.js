6789d79b57141775ab9d85f47351a0ac9845931e
Victoria Viana <victoriagarcia963@gmail.com>
2022-07-04 11:16:39 -0300

    fix: height about page

--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Container = styled.section`
   margin-top: 1rem;
   width: 100%;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
