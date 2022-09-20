505d5d5274619482bdbb80913ff3ef3b58453a41
victoriavianx <victoriagarcia963@gmail.com>
2022-09-20 14:46:31 -0300

    update: menu position

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,43 +1,44 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  position: fixed;
-  bottom: 1rem;
-  background-color: var(--hover-color);
-  width: 90%;
-  border-radius: 4rem;
-  padding: 1rem 2.25rem;
-  margin: auto;
-  margin-bottom: 1rem;
-  max-width: 370px;
+  display: none;
 
-  ul {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  @media (min-width: 900px) {
+    display: block;
+    position: fixed;
+    bottom: 1rem;
+    background-color: var(--hover-color);
+    width: 90%;
+    border-radius: 4rem;
+    padding: 1rem 2.25rem;
+    margin: auto;
+    margin-bottom: 2rem;
+    max-width: 370px;
 
-    li {
-      a {
-        display: flex;
-        color: #fde6ff;
-        font-size: 1.25rem;
-        padding: 0.4rem;
-        border-radius: 5rem;
+    ul {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
 
-        :hover {
-          transition: 0.5s;
-          background-color: rgba(255, 255, 255, 0.3);
-          //background-color: var(--background-dark-mode);
-        }
+      li {
+        a {
+          display: flex;
+          color: #fde6ff;
+          font-size: 1.25rem;
+          padding: 0.4rem;
+          border-radius: 5rem;
 
-        :focus {
-          background-color: var(--background-dark-mode);
+          :hover {
+            transition: 0.5s;
+            background-color: rgba(255, 255, 255, 0.3);
+            //background-color: var(--background-dark-mode);
+          }
+
+          :focus {
+            background-color: var(--background-dark-mode);
+          }
         }
       }
     }
-  }
-
-  @media (min-width: 768px) {
-    margin-bottom: 1rem;
   }
 `;
