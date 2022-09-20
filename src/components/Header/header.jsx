c4be2db4570a9e2112a0cf030c3e05f9cdbfcaf0
victoriavianx <victoriagarcia963@gmail.com>
2022-09-20 14:46:08 -0300

    feat: mobile menu

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,7 +1,21 @@
-import { BiMoon, BiSun } from "react-icons/bi";
-import { Container, Logo, NavBox, ThemeButton } from "./styles";
-import { ThemeContext } from "../../providers/Theme/theme";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { BiMoon, BiSun } from "react-icons/bi";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiHome, FiUser, FiBook } from "react-icons/fi";
+import { BiBriefcase } from "react-icons/bi";
+import { Menu } from "@chakra-ui/react";
+import {
+  Container,
+  IconButton,
+  Item,
+  List,
+  Logo,
+  NavBox,
+  SideMenu,
+  ThemeButton,
+} from "./styles";
+import { ThemeContext } from "../../providers/Theme/theme";
 
 const Header = () => {
   const { changeTheme, currentTheme, setCurrentTheme } =
@@ -15,15 +29,48 @@ const Header = () => {
     <Container>
       <NavBox>
         <Logo href="/">{"< Victoria />"}</Logo>
-        {currentTheme === "light" ? (
-          <ThemeButton onClick={handleTheme}>
-            <BiMoon />
-          </ThemeButton>
-        ) : (
-          <ThemeButton onClick={handleTheme}>
-            <BiSun />
-          </ThemeButton>
-        )}
+        <SideMenu>
+          <Menu>
+            <IconButton>
+              <AiOutlineMenu />
+            </IconButton>
+            <List>
+              <Link to={"/"}>
+                <Item>
+                  <FiHome />
+                  Home
+                </Item>
+              </Link>
+              <Link to={"/sobre"}>
+                <Item>
+                  <FiUser />
+                  Sobre
+                </Item>
+              </Link>
+              <Link to={"/skills"}>
+                <Item>
+                  <FiBook />
+                  Habilidades
+                </Item>
+              </Link>
+              <Link to={"/projetos"}>
+                <Item>
+                  <BiBriefcase />
+                  Projetos
+                </Item>
+              </Link>
+            </List>
+          </Menu>
+          {currentTheme === "light" ? (
+            <ThemeButton onClick={handleTheme}>
+              <BiMoon />
+            </ThemeButton>
+          ) : (
+            <ThemeButton onClick={handleTheme}>
+              <BiSun />
+            </ThemeButton>
+          )}
+        </SideMenu>
       </NavBox>
     </Container>
   );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 
 export const Container = styled.header`
   display: flex;
@@ -30,6 +31,10 @@ export const Logo = styled.a`
   }
 `;
 
+export const SideMenu = styled.div`
+  display: flex;
+`;
+
 export const ThemeButton = styled.button`
   background-color: var(--hover-color);
   border-radius: 100%;
@@ -38,7 +43,48 @@ export const ThemeButton = styled.button`
 
   svg {
     color: var(--font-light-color);
-    margin-top: 0.4rem;
+    margin: 0.4rem;
     font-size: 1.25rem;
+  }
+`;
+
+export const IconButton = styled(MenuButton)`
+  visibility: visible;
+  margin-right: 1rem;
+
+  svg {
+    width: 30px;
+    height: 30px;
+  }
+
+  @media (min-width: 900px) {
+    visibility: hidden;
+  }
+`;
+
+export const List = styled(MenuList)`
+  width: 50vw;
+  height: 230px;
+  background-color: ${(props) => props.theme.div.backgroundColor};
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  max-width: 250px;
+
+  button {
+    color: ${(props) => props.theme.button.color};
+  }
+`;
+
+export const Item = styled(MenuItem)`
+  padding-top: 1.5rem;
+  padding-left: 1rem;
+  height: 50px;
+
+  svg {
+    margin-right: 0.8rem;
+  }
+
+  :hover {
+    color: var(--hover-color);
   }
 `;
